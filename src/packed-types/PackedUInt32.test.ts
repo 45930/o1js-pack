2fecd0e1d1031faeb417c4d8f3c5795aecf075dc
Coby <contact@45930.xyz>
2023-07-14 11:46:07 -0400

    working on extending provable type for uints

--- src/packed-types/PackedUInt32.test.ts
+++ src/packed-types/PackedUInt32.test.ts
@@ -1,34 +1,38 @@
-import { Field, Struct, UInt32 } from 'snarkyjs';
+import { Circuit, Field, Provable, Struct, UInt32 } from 'snarkyjs';
 import { PackedUInt32Factory } from './PackedUInt32';
 
 describe('PackedUInt32', () => {
   it('packs and unpacks 2 UInt32 values', async () => {
+    const uints = [UInt32.from(67), UInt32.from(2n ** 32n - 1n)];
     const PackedUInt32_2 = PackedUInt32Factory(2);
 
-    const packedUInt32_2 = new PackedUInt32_2([
-      UInt32.from(67),
-      UInt32.from(2n ** 32n - 1n),
-    ]);
-    const f = packedUInt32_2.toFields()[0];
+    const packedUInt32_2 = new PackedUInt32_2(
+      PackedUInt32_2.pack(uints),
+      uints
+    );
+    const f = packedUInt32_2.packed;
     const unpacked = PackedUInt32_2.unpack(f);
 
     expect(unpacked[0].toBigint()).toBe(67n);
     expect(unpacked[1].toBigint()).toBe(2n ** 32n - 1n);
-    // Why isn't my type provable?
-    //   expect(packedUInt32_2.sizeInFields()).toBe(1);
+    expect(unpacked[0].toBigint()).toBe(packedUInt32_2.uints[0].toBigint());
+    expect(unpacked[1].toBigint()).toBe(packedUInt32_2.uints[1].toBigint());
   });
   it('packs and unpacks 5 UInt32 values', async () => {
-    const PackedUInt32_5 = PackedUInt32Factory(5);
-
-    const packedUInt32_2 = new PackedUInt32_5([
+    const uints = [
       UInt32.from(67),
       UInt32.from(2n ** 32n - 1n),
       UInt32.from(0),
       UInt32.from(0),
       UInt32.from(128),
-    ]);
+    ];
+    const PackedUInt32_5 = PackedUInt32Factory(5);
 
-    const f = packedUInt32_2.toFields()[0];
+    const packedUInt32_5 = new PackedUInt32_5(
+      PackedUInt32_5.pack(uints),
+      uints
+    );
+    const f = packedUInt32_5.packed;
     const unpacked = PackedUInt32_5.unpack(f);
 
     expect(unpacked[0].toBigint()).toBe(67n);
@@ -36,5 +40,51 @@ describe('PackedUInt32', () => {
     expect(unpacked[2].toBigint()).toBe(0n);
     expect(unpacked[3].toBigint()).toBe(0n);
     expect(unpacked[4].toBigint()).toBe(128n);
+
+    expect(unpacked[0].toBigint()).toBe(packedUInt32_5.uints[0].toBigint());
+    expect(unpacked[1].toBigint()).toBe(packedUInt32_5.uints[1].toBigint());
+    expect(unpacked[2].toBigint()).toBe(packedUInt32_5.uints[2].toBigint());
+    expect(unpacked[3].toBigint()).toBe(packedUInt32_5.uints[3].toBigint());
+    expect(unpacked[4].toBigint()).toBe(packedUInt32_5.uints[4].toBigint());
+  });
+  it('Validates with #check', () => {
+    const uints = [
+      UInt32.from(67),
+      UInt32.from(2n ** 32n - 1n),
+      UInt32.from(0),
+      UInt32.from(0),
+      UInt32.from(128),
+    ];
+    const PackedUInt32_5 = PackedUInt32Factory(5);
+
+    Provable.runAndCheck(() => {
+      const packedUInt32_5 = new PackedUInt32_5(
+        PackedUInt32_5.pack(uints),
+        uints
+      );
+    });
+  });
+  it('Throws with invalid #check', () => {
+    const uints = [
+      UInt32.from(67),
+      UInt32.from(2n ** 32n - 1n),
+      UInt32.from(0),
+      UInt32.from(0),
+      UInt32.from(128),
+    ];
+    const PackedUInt32_2 = PackedUInt32Factory(2);
+    const PackedUInt32_5 = PackedUInt32Factory(5);
+    const packedUInt32_5 = new PackedUInt32_5(
+      PackedUInt32_5.pack(uints),
+      uints
+    );
+
+    // Provable.runAndCheck(() => {
+    const packedUInt32_2 = new PackedUInt32_2(
+      PackedUInt32_5.pack(uints).add(5),
+      uints
+    );
+    PackedUInt32_5.check(packedUInt32_2);
+    // })
   });
 });
--- src/packed-types/PackedUInt32.ts
+++ src/packed-types/PackedUInt32.ts
@@ -1,35 +1,57 @@
-import { Field, Provable, Struct, UInt32 } from 'snarkyjs';
+import { Field, Provable, Struct, UInt32, provable } from 'snarkyjs';
 
 export function PackedUInt32Factory(l: number) {
   class PackedUInt32_ extends Struct({
-    values: Provable.Array(UInt32, l),
+    packed: Field,
   }) {
+    static type = provable({ packed: Field }, {});
     static l: number = l;
+    packed: Field;
+    uints: UInt32[];
 
-    constructor(values: UInt32[]) {
-      super({ values });
+    constructor(packed: Field, uints: UInt32[]) {
+      super({ packed });
+      this.uints = uints;
     }
 
-    toFields(): Field[] {
-      let f = Field(0);
-      for (let i = 0; i < l; i++) {
-        const c = (2n ** 32n) ** BigInt(i);
-        f = f.add(this.values[i].toBigint() * c);
-      }
-      return [f];
-    }
-
-    static unpack(packed: Field): UInt32[] {
-      const ret = Provable.witness(Provable.Array(UInt32, l), () => {
+    /**
+     * Unpacks a Field into its component UInt32 parts
+     * @param value
+     * @returns the unpacked auxilliary data used to pack the value
+     */
+    static toAuxiliary(value?: { packed: Field } | undefined): any[] {
+      const auxiliary = Provable.witness(Provable.Array(UInt32, l), () => {
         let uints: bigint[] = [];
-        let packedN = packed.toBigInt();
+        let packedN = value?.packed.toBigInt() || 0n;
         for (let i = 0; i < l; i++) {
           uints[i] = packedN & ((1n << 32n) - 1n);
           packedN >>= 32n;
         }
         return uints.map((x) => UInt32.from(x));
       });
-      return ret;
+      return auxiliary;
+    }
+
+    static pack(uints: UInt32[]): Field {
+      let f = Field(0);
+      for (let i = 0; i < l; i++) {
+        const c = Field((2n ** 32n) ** BigInt(i));
+        f = f.add(uints[i].value.mul(c));
+      }
+      return f;
+    }
+
+    static unpack(f: Field): UInt32[] {
+      return this.toAuxiliary({ packed: f });
+    }
+
+    /**
+     * In-Circuit verifiaction of the packed Field
+     */
+    static check(value: { packed: Field }) {
+      const unpacked = this.toAuxiliary(value);
+      const packed = this.pack(unpacked);
+      packed.assertEquals(value.packed);
     }
   }
   return PackedUInt32_;
