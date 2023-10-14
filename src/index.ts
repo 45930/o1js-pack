63a70fb606cc48f6c409051b571ff42048428aed
Coby <contact@45930.xyz>
2023-10-14 16:29:21 -0400

    expose MultiPackedStringFactory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { PackedBoolFactory } from './lib/packed-types/PackedBool.js';
 import { PackedUInt32Factory } from './lib/packed-types/PackedUInt32.js';
 import { PackingPlant, MultiPackingPlant } from './lib/PackingPlant.js';
-import { PackedStringFactory } from './lib/packed-types/PackedString.js';
+import {
+  PackedStringFactory,
+  MultiPackedStringFactory,
+} from './lib/packed-types/PackedString.js';
 
 export {
   PackedBoolFactory,
   PackedStringFactory,
+  MultiPackedStringFactory,
   PackedUInt32Factory,
   PackingPlant,
   MultiPackingPlant,
